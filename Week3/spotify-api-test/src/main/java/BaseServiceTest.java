08519e0b497682900e9d18a631b2041825b5ba65
fatossgorur <55894683+fatossgorur@users.noreply.github.com>
2021-12-05 02:29:45 +0300

    Delete Week3/spotify-api-test directory

--- Week3/spotify-api-test/src/main/java/BaseServiceTest.java
+++
@@ -1,9 +0,0 @@
-import service.Playlist;
-import service.Search;
-import service.UsersProfile;
-
-public class BaseServiceTest {
-    UsersProfile usersProfile = new UsersProfile();
-    Playlist playlist = new Playlist();
-    Search search = new Search();
-}
